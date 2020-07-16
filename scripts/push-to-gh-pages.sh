8aff6290b9c77619c64350ffe12bc06e514a9e09
ahenteti <ahmad.henteti@gmail.com>
2020-07-16 14:05:24 +0200

    update push-to-gh-pages.sh script

--- scripts/push-to-gh-pages.sh
+++ scripts/push-to-gh-pages.sh
@@ -7,4 +7,8 @@ cd $ROOT_DIR
 ng build
 git checkout gh-pages
 cp dist/ahenteti-slides/* .
+sed -i 's:src=":src="ahenteti-slides/:g' index.html
+sed -i 's:href="favicon.png":href="ahenteti-slides/favicon.png":g' index.html
 git commit -am "new website version"
+git push
+git checkout master
