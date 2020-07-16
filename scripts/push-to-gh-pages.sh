e222be871dd576d9c807a70d024af5f75c36e111
ahenteti <ahmad.henteti@gmail.com>
2020-07-16 14:23:36 +0200

    update push-to-gh-pages.sh script -- build project with --prod flag

--- scripts/push-to-gh-pages.sh
+++ scripts/push-to-gh-pages.sh
@@ -4,11 +4,18 @@ set -e
 ROOT_DIR=$(dirname "$0")"/../"
 rm -rf dist
 cd $ROOT_DIR
-ng build
+ng build --prod
 git checkout gh-pages
-cp dist/ahenteti-slides/* .
+rm index* main* polyfills* runtime* styles*
+cp dist/ahenteti-slides/index* .
+cp dist/ahenteti-slides/main* .
+cp dist/ahenteti-slides/polyfills* .
+cp dist/ahenteti-slides/runtime* .
+cp dist/ahenteti-slides/styles* .
 sed -i 's:src=":src="ahenteti-slides/:g' index.html
-sed -i 's:href="favicon.png":href="ahenteti-slides/favicon.png":g' index.html
-git commit -am "new website version"
+sed -i 's:href="favicon:href="ahenteti-slides/favicon:g' index.html
+sed -i 's:href="styles:href="ahenteti-slides/styles:g' index.html
+git add .
+git commit -m "new website version"
 git push
 git checkout master
