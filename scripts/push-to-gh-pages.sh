42f52a2128851b50270b4f9c3e95345606d98850
ahenteti <ahmad.henteti@gmail.com>
2020-07-16 13:37:18 +0200

    update push-to-gh-pages script

--- scripts/push-to-gh-pages.sh
+++ scripts/push-to-gh-pages.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
+set -e
 ROOT_DIR=$(dirname "$0")"/../"
 rm -rf dist
 cd $ROOT_DIR
 ng build
+git checkout gh-pages
+cp dist/ahenteti-slides/* .
+git commit -am "new website version"
