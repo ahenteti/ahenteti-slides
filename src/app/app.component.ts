596d59a6fd56409f373677a7f47cc6d0c913bd2d
ahenteti <ahmad.henteti@gmail.com>
2020-07-16 15:14:22 +0200

    add reveal.js dependency

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+
+import Reveal from 'reveal.js';
+import RevealZoom from 'reveal.js/plugin/zoom/zoom.js';
+import RevealNotes from 'reveal.js/plugin/notes/notes.js';
+import RevealSearch from 'reveal.js/plugin/search/search.js';
+import RevealMarkdown from 'reveal.js/plugin/markdown/markdown.js';
+import RevealHighlight from 'reveal.js/plugin/highlight/highlight.js';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'ahenteti-slides';
+export class AppComponent implements AfterViewInit {
+  ngAfterViewInit(): void {
+    Reveal.initialize({
+      plugins: [
+        RevealZoom,
+        RevealNotes,
+        RevealSearch,
+        RevealMarkdown,
+        RevealHighlight,
+      ],
+    });
+  }
 }
